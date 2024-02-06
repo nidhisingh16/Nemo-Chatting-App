8351ff0c92a3adba365f890ef80f23b848567b59
nidhisingh16 <nidhisinghwgs@gmail.com>
2024-02-06 12:06:07 +0530

    Login Design Completed

---
+++ app/src/main/java/com/example/chat_application_nemo/SignUp.kt
@@ -0,0 +1,11 @@
+package com.example.chat_application_nemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
