9ddbbf2de54a2ebbcdc7d73ffc2d314f1c8a36c7
nidhisingh16 <nidhisinghwgs@gmail.com>
2024-02-06 18:55:40 +0530

    Firebase Authentication Activated

--- app/src/main/java/com/example/chat_application_nemo/Login.kt
+++ app/src/main/java/com/example/chat_application_nemo/Login.kt
@@ -1,11 +1,29 @@
 package com.example.chat_application_nemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.Button
+import android.widget.EditText
 
 class Login : AppCompatActivity() {
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignUp: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
     }
 }
