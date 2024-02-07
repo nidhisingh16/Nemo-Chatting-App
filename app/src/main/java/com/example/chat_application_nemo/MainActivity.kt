6087f1fd5db8e98c0e042fc88f0c21eabacc89be
nidhisingh16 <nidhisinghwgs@gmail.com>
2024-02-07 13:33:19 +0530

    Adding Users to Realtime Database

--- app/src/main/java/com/example/chat_application_nemo/MainActivity.kt
+++ app/src/main/java/com/example/chat_application_nemo/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.chat_application_nemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var userRecyclerView: RecyclerView
     private lateinit var userList: ArrayList<User>
     private lateinit var adapter: UserAdapter
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAuth = FirebaseAuth.getInstance()
 
         userList = ArrayList()
         adapter = UserAdapter(this, userList)
